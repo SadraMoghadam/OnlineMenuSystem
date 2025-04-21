fee549d0d205f9f71b5ba7a333be5c6ec3b724d4
DESKTOP-D8N6CJF\Sadra <sadra_h_m@outlook.com>
2025-04-21 19:31:01 +0200

    Adding the UI widget and Accessing our Subsystem

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/MultiplayerSessionsSubsystem.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "OnlineSubsystem.h"
 #include "MultiplayerSessionsSubsystem.h"
+#include "OnlineSubsystem.h"
 
 
 UMultiplayerSessionsSubsystem::UMultiplayerSessionsSubsystem():
